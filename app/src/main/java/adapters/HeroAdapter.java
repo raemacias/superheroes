9cecb69dce185118a44f03b0d0e02b83cc7a99ce
Rae <raemmacias@gmail.com>
2018-11-07 19:23:23 -0700

    initial commit

---
+++ app/src/main/java/adapters/HeroAdapter.java
@@ -0,0 +1,108 @@
+package adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.raemacias.superheroes.R;
+
+import java.util.List;
+
+import models.Hero;
+
+public class HeroAdapter extends RecyclerView.Adapter<HeroAdapter.HeroViewHolder> {
+
+
+    private List<Hero> heroList;
+    private Context context;
+
+    private static int currentPosition = 0;
+
+    public HeroAdapter(Context context, List<Hero> heroList) {
+
+        this.context = context;
+        this.heroList = heroList;
+    }
+
+    @NonNull
+    @Override
+    public HeroViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_layout_heroes, parent, false);
+        return new HeroViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(final HeroViewHolder holder, final int position) {
+        Hero hero = heroList.get(position);
+        holder.textViewName.setText(hero.getName());
+        holder.textViewRealName.setText(hero.getRealname());
+        holder.textViewTeam.setText(hero.getTeam());
+        holder.textViewFirstAppearance.setText(hero.getFirstappearance());
+        holder.textViewCreatedBy.setText(hero.getCreatedby());
+        holder.textViewPublisher.setText(hero.getPublisher());
+        holder.textViewBio.setText(hero.getBio().trim());
+
+        Glide.with(context).load(hero.getImageurl()).into(holder.imageView);
+        holder.linearLayout.setVisibility(View.GONE);
+
+        //if the position is equals to the item position which is to be expanded
+        if (currentPosition == position) {
+            //creating an animation
+            Animation slideDown = AnimationUtils.loadAnimation(context, R.anim.slide_down);
+
+            //toggling visibility
+            holder.linearLayout.setVisibility(View.VISIBLE);
+
+            //adding sliding effect
+            holder.linearLayout.startAnimation(slideDown);
+        }
+
+        holder.textViewName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //getting the position of the item to expand it
+                currentPosition = position;
+
+                //reloding the list
+                notifyDataSetChanged();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return heroList.size();
+    }
+
+    class HeroViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewName, textViewRealName, textViewTeam, textViewFirstAppearance,
+                textViewCreatedBy, textViewPublisher, textViewBio;
+        ImageView imageView;
+        LinearLayout linearLayout;
+
+        HeroViewHolder(View itemView) {
+            super(itemView);
+
+            textViewName = itemView.findViewById(R.id.textViewName);
+            textViewRealName = itemView.findViewById(R.id.textViewRealName);
+            textViewTeam = itemView.findViewById(R.id.textViewTeam);
+            textViewFirstAppearance = itemView.findViewById(R.id.textViewFirstAppearance);
+            textViewCreatedBy = itemView.findViewById(R.id.textViewCreatedBy);
+            textViewPublisher = itemView.findViewById(R.id.textViewPublisher);
+            textViewBio = itemView.findViewById(R.id.textViewBio);
+            imageView = itemView.findViewById(R.id.imageView);
+
+            linearLayout = itemView.findViewById(R.id.linearLayout);
+        }
+    }
+}
---
+++ app/src/main/java/com/raemacias/superheroes/MainActivity.java
@@ -0,0 +1,127 @@
+package com.raemacias.superheroes;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adapters.HeroAdapter;
+import models.Hero;
+import network.Api;
+import okhttp3.Request;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+//This code was completed by following the tutorial at https://www.simplifiedcoding.net/expandable-recyclerview-android/
+
+public class MainActivity extends AppCompatActivity {
+
+    final String URL_GET_DATA = "https://simplifiedcoding.net/demos/marvel/";
+
+    AdView mAdView;
+    RecyclerView recyclerView;
+    HeroAdapter adapter;
+    List<Hero> heroList;
+
+    private String TAG = "Main Activity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        heroList = new ArrayList<>();
+
+        //MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+        //To show banner ads
+        AdView adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER);
+        //This is the test ads id
+        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+
+        //calling the method to display the heroes
+//        getHeroes();
+        loadHeroes();
+    }
+
+    private void loadHeroes() {
+
+        StringRequest stringRequest = new StringRequest(com.android.volley.Request.Method.GET, URL_GET_DATA,
+                new com.android.volley.Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject obj = jsonArray.getJSONObject(i);
+
+                                Hero hero = new Hero(
+                                        obj.getString("name"),
+                                        obj.getString("realname"),
+                                        obj.getString("team"),
+                                        obj.getString("firstappearance"),
+                                        obj.getString("createdby"),
+                                        obj.getString("publisher"),
+                                        obj.getString("imageurl"),
+                                        obj.getString("bio")
+                                );
+
+                                heroList.add(hero);
+                            }
+
+                            adapter = new HeroAdapter(getApplicationContext(), heroList);
+                            recyclerView.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+}
