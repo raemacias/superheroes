5866821cf6abb96c0d83d03db6dc6ce1d620661d
Rae <raemmacias@gmail.com>
2018-11-08 18:59:19 -0700

    Reversed some changes and added Firebase for analytics.

--- app/src/main/java/adapters/HeroAdapter.java
+++ app/src/main/java/adapters/HeroAdapter.java
@@ -21,32 +21,25 @@ import com.raemacias.superheroes.R;
 
 import java.util.List;
 
-import activities.DetailActivity;
 import models.Hero;
 
 // This code has been adapted from www.learn2crack.com
 // and Simplified Coding
 
-public class HeroAdapter extends RecyclerView.Adapter<HeroAdapter.HeroViewHolder> implements OnLikeListener {
-
+public class HeroAdapter extends RecyclerView.Adapter<HeroAdapter.HeroViewHolder> {
     private List<Hero> heroList;
     private Context context;
-
     private static int currentPosition = 0;
-
     public HeroAdapter(Context context, List<Hero> heroList) {
-
         this.context = context;
         this.heroList = heroList;
     }
-
     @NonNull
     @Override
     public HeroViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_layout_heroes, parent, false);
         return new HeroViewHolder(v);
     }
-
     @Override
     public void onBindViewHolder(final HeroViewHolder holder, final int position) {
         Hero hero = heroList.get(position);
@@ -57,63 +50,38 @@ public class HeroAdapter extends RecyclerView.Adapter<HeroAdapter.HeroViewHolder
         holder.textViewCreatedBy.setText(hero.getCreatedby());
         holder.textViewPublisher.setText(hero.getPublisher());
         holder.textViewBio.setText(hero.getBio().trim());
-
         Glide.with(context).load(hero.getImageurl()).into(holder.imageView);
         holder.linearLayout.setVisibility(View.GONE);
-
         //if the position is equals to the item position which is to be expanded
         if (currentPosition == position) {
             //creating an animation
             Animation slideDown = AnimationUtils.loadAnimation(context, R.anim.slide_down);
-
             //toggling visibility
             holder.linearLayout.setVisibility(View.VISIBLE);
-
             //adding sliding effect
             holder.linearLayout.startAnimation(slideDown);
         }
-
         holder.textViewName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 //getting the position of the item to expand it
                 currentPosition = position;
-
-                //reloading the list
+                //reloding the list
                 notifyDataSetChanged();
             }
         });
     }
-    public void setId(List<Hero> favoriteResults){
-        heroList = favoriteResults;
-        notifyDataSetChanged();
-    }
-
     @Override
     public int getItemCount() {
         return heroList.size();
     }
-
-    @Override
-    public void liked(LikeButton likeButton) {
-
-    }
-
-    @Override
-    public void unLiked(LikeButton likeButton) {
-
-    }
-
     class HeroViewHolder extends RecyclerView.ViewHolder {
         TextView textViewName, textViewRealName, textViewTeam, textViewFirstAppearance,
                 textViewCreatedBy, textViewPublisher, textViewBio;
         ImageView imageView;
         LinearLayout linearLayout;
-
         HeroViewHolder(View itemView) {
             super(itemView);
-
             textViewName = itemView.findViewById(R.id.textViewName);
             textViewRealName = itemView.findViewById(R.id.textViewRealName);
             textViewTeam = itemView.findViewById(R.id.textViewTeam);
@@ -122,31 +90,7 @@ public class HeroAdapter extends RecyclerView.Adapter<HeroAdapter.HeroViewHolder
             textViewPublisher = itemView.findViewById(R.id.textViewPublisher);
             textViewBio = itemView.findViewById(R.id.textViewBio);
             imageView = itemView.findViewById(R.id.imageView);
-
             linearLayout = itemView.findViewById(R.id.linearLayout);
-
-            itemView.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-
-                    int pos = getAdapterPosition();
-                    if (pos != RecyclerView.NO_POSITION) {
-                        Hero clickedDataItem = heroList.get(pos);
-                        Intent intent = new Intent(context, DetailActivity.class);
-                        intent.putExtra("name", heroList.get(pos).getName());
-                        intent.putExtra("realname", heroList.get(pos).getRealname());
-                        intent.putExtra("team", heroList.get(pos).getTeam());
-                        intent.putExtra("firstappearance", heroList.get(pos).getFirstappearance());
-                        intent.putExtra("createdby", heroList.get(pos).getCreatedby());
-                        intent.putExtra("publisher", heroList.get(pos).getPublisher());
-                        intent.putExtra("bio", heroList.get(pos).getBio());
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        context.startActivity(intent);
-                        Toast.makeText(v.getContext(), "You clicked " + clickedDataItem.getName(), Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
         }
     }
 }
